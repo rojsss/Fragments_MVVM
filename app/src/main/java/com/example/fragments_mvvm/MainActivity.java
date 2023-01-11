4e094543d37349fd3070f1377e2b2d3a95236810
rojsss <mathiasrojas@ceroca.cat>
2023-01-11 20:27:02 +0100

    Projecte creat Carregant fragment 1 i 2

--- app/src/main/java/com/example/fragments_mvvm/MainActivity.java
+++ app/src/main/java/com/example/fragments_mvvm/MainActivity.java
@@ -2,14 +2,43 @@
 package com.example.fragments_mvvm;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mBtnCarregarFragment1;
+    private Button mBtnCarregarFragment2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBtnCarregarFragment1 = findViewById(R.id.BTN_Fragment1);
+        mBtnCarregarFragment2 = findViewById(R.id.BTN_Fragment2);
+
+        mBtnCarregarFragment1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Cargamos el fragment 1:
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.FL_ContingutFragments, new Fragment1())
+                        .commit();
+
+            }
+
+        });
+        mBtnCarregarFragment2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.FL_ContingutFragments, new Fragment2())
+                        .commit();
+            }
+        });
     }
 }
